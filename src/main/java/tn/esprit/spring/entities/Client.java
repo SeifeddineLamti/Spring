c92350ba1e60f2c060b9dcbe761ca979490a7d91
SeifeddineLamti <seifeddine.lamti@esprit.tn>
2025-02-16 18:23:26 +0100

    Merge branch 'master'

--- src/main/java/tn/esprit/spring/entities/Client.java
+++ src/main/java/tn/esprit/spring/entities/Client.java
@@ -12,6 +12,7 @@ public class Client implements Serializable  { //protection de donnee en bytes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idClient;
+    //aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     private String identifiant;
     @Temporal(TemporalType.DATE)
     private Date datePremiereVisite;
